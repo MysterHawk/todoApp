8e4c126b66e7ec53aed0e29a614d45c056679c05
Antonio Gagliarducci <gagliarducci.antonio@posteo.net>
2020-08-30 17:23:39 +0200

    Initial commit

---
+++ fullscreen.nim
@@ -0,0 +1,7 @@
+echo "Press Esc or Ctrl-C to quit"
+echo "==============="
+echo "NIM INDUSTRIES (TM) TERMINAL PROTOCOL, Todo List Application version 0.01V \n"
+echo "Please enter your credentials:"
+echo "username : "
+let username:string = readLine(stdin)
+echo "Your username is ", username
